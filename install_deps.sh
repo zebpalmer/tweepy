e6029952ea55faf56dec8f6a791da22200d43910
Aaron Hill <aa1ronham@gmail.com>
2014-03-10 18:17:46 -0400

    Install requirements.txt

--- install_deps.sh
+++ install_deps.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 if [[ $TRAVIS_PYTHON_VERSION == 2* ]]; then
-  pip install -r test_requirements_py2.txt
+  pip install -r test_requirements_py2.txt -r requirements.txt
 elif [[ $TRAVIS_PYTHON_VERSION == 3* ]]; then
-  pip install -r test_requirements_py3.txt
+  pip install -r test_requirements_py3.txt -r requirements.txt
 fi
