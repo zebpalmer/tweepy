8ec6f69da90099593feeed5d514878e85ada8770
Aaron Hill <aa1ronham@gmail.com>
2014-03-11 16:17:12 -0400

    Use six.text_type for reason in TweepError

--- tweepy/error.py
+++ tweepy/error.py
@@ -2,11 +2,13 @@
 # Copyright 2009-2010 Joshua Roesslein
 # See LICENSE for details.
 
+import six
+
 class TweepError(Exception):
     """Tweepy exception"""
 
     def __init__(self, reason, response=None):
-        self.reason = unicode(reason)
+        self.reason = six.text_type(reason)
         self.response = response
         Exception.__init__(self, reason)
 
